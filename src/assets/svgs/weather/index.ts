88d9f8b7a055ee630658620f7092ceb4c690d2c6
zellytozelly <zellytozelly@gmail.com>
2022-05-09 15:06:00 +0900

    Feat: Add Weather Color and Icon

--- src/assets/svgs/weather/index.ts
+++ src/assets/svgs/weather/index.ts
@@ -1,2 +1,8 @@
 export { ReactComponent as CloudIcon } from './cloud.svg'
 export { ReactComponent as RainIcon } from './rain.svg'
+export { ReactComponent as ClearIcon } from './sun.svg'
+export { ReactComponent as AtmosphereIcon } from './fog.svg'
+export { ReactComponent as SnowIcon } from './snow.svg'
+export { ReactComponent as DrizzleIcon } from './drizzle.svg'
+export { ReactComponent as ThunderstormIcon } from './lightning.svg'
+
--- src/utils/weather.ts
+++ src/utils/weather.ts
@@ -1,10 +1,25 @@
-import { CloudIcon, RainIcon } from 'assets/svgs/weather'
+import { CloudIcon, RainIcon, ClearIcon, AtmosphereIcon, SnowIcon, DrizzleIcon, ThunderstormIcon } from 'assets/svgs/weather'
 
 export const getWeatherIcon = (desc: string) => {
   let weatherIconName = RainIcon
   switch (desc) {
     case 'Clouds':
       weatherIconName = CloudIcon
+      break
+    case 'Clear':
+      weatherIconName = ClearIcon
+      break
+    case 'Atmosphere':
+      weatherIconName = AtmosphereIcon
+      break
+    case 'Snow':
+      weatherIconName = SnowIcon
+      break
+    case 'Drizzle':
+      weatherIconName = DrizzleIcon
+      break
+    case 'Thunderstorm':
+      weatherIconName = ThunderstormIcon
       break
     default:
       break
